e39308bc0812f1e9a05bacb5f369d61b912b2015
pohuyproyebali <mrkololoshka413@gmail.com>
2024-09-12 10:13:37 +0700

    Первый коммит. Создание проекта и репозитория. Создал модели для приложения "курсы"

---
+++ courses/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+admin.site.register(models.Course)
+admin.site.register(models.Company)
+admin.site.register(models.User)
+admin.site.register(models.Speaker)
+admin.site.register(models.Skill)
+admin.site.register(models.Lesson)
+admin.site.register(models.VideoToLesson)
+admin.site.register(models.ImageToTextLesson)
+admin.site.register(models.CourseToUser)
+admin.site.register(models.LessonToUser)
+admin.site.register(models.TextToLesson)
+admin.site.register(models.SpeakerToCourse)
+admin.site.register(models.ProgramUnit)
---
+++ courses/models.py
@@ -0,0 +1,121 @@
+from django.db import models
+
+# Create your models here.
+
+class Company(models.Model):
+    """ Модель для компаний """
+    name = models.CharField(max_length=100)
+    video_link = models.CharField(max_length=250)
+    main_text = models.CharField(max_length=450)
+    secondary_text = models.CharField(max_length=450)
+    logo = models.ImageField(upload_to='course_images/')
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    """ Модель для курсов """
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(to=Company, on_delete=models.CASCADE)
+    about = models.TextField()
+    skills = models.TextField()
+    preview_image = models.ImageField(upload_to='course_images/')
+    image = models.ImageField(upload_to='course_images/')
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    """ Модель для пользователя """
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Skill(models.Model):
+    """ Модель для навыков получаемых в ходе прохождения курса """
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='course_images/')
+    about = models.TextField()
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    """ Модель для уроков в конкретном курсе """
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class VideoToLesson(models.Model):
+    """ Модель для видеоуроков """
+    lesson = models.ForeignKey(to=Lesson, on_delete=models.CASCADE)
+    video = models.CharField(max_length=250)
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class TextToLesson(models.Model):
+    """ Модель для текстовых уроков """
+    lesson = models.ForeignKey(to=Lesson, on_delete=models.CASCADE)
+    text = models.TextField()
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class ImageToTextLesson(models.Model):
+    """ Модель для изображений в уроке """
+    text_lesson = models.ForeignKey(to=TextToLesson, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='course_images/')
+
+    def __str__(self):
+        return 'изображение для' + self.text_lesson.name
+
+class LessonToUser(models.Model):
+    """ Модель для соединения уроков с пользователем и отметки выполнения """
+    lesson = models.ForeignKey(to=Lesson, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.name + ' - ' + self.lesson.name + 'выполнено' if self.completed else 'не выполнено'
+
+class CourseToUser(models.Model):
+    """ Модель для соединения различных курсов с конкретным пользователем """
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name + ' - ' + self.course.name
+
+class Speaker(models.Model):
+    """ Модель для спикеров """
+    name = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='course_images/')
+    def __str__(self):
+        return self.name
+
+class SpeakerToCourse(models.Model):
+    """ Модель для привязки различных спикеров к конкретному курсу """
+    speaker = models.ForeignKey(to=Speaker, on_delete=models.CASCADE)
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.speaker.name + ' - ' + self.course.name
+
+class ProgramUnit(models.Model):
+    """ Модель для этапов программы конкретного курса """
+    text = models.TextField()
+    speaker = models.ForeignKey(to=Speaker, on_delete=models.CASCADE)
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
